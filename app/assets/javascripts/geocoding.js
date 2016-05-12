9e79c06b339360e718e915cf4642b087317ff677
Débora Setton <setton.debora@gmail.com>
2016-05-12 12:04:47 -0300

    Add geocoding "Hello, world" example

---
+++ app/assets/javascripts/geocoding.js
@@ -0,0 +1,24 @@
+/**
+ * Example from: https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple
+ */
+
+$(function () {
+
+  function geocodeAddress(address) {
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  $("#address-submit").click(function() {
+    geocodeAddress($("#address").val());
+  });
+});
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,9 @@
-var map;
+var map, geocoder;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
+  geocoder = new google.maps.Geocoder();
 }
