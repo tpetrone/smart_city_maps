d79adaed56e45bafb55ca83e8e236ffd4fa6aca2
Nury Yanque <yuleny.aryan@gmail.com>
2016-06-06 13:04:40 +0000

    Fix markers spot icon by its state, add filters and a legend

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,10 @@
+
+var markerGroups = {
+  "available": [],
+  "occupied": [],
+  "defected": []
+};
+
 function main() {
 
   // Create map instance
@@ -10,16 +17,45 @@ function main() {
   });
 
   map.enableAutocomplete($('#search-field')[0]);
-
+  var states = { '-1': 'defected', '0': 'available', '1': 'occupied'};
   $.getJSON("/json/spots.json").done(function(data) {
     if (data.spots.length > 0) {
       for (i = 0; i < data.spots.length; i++) {
         spot = data.spots[i];
-        marker = new GmapMarker(map, spot);
+        console.log(spot);
+        gmarker = new GmapMarker(map, spot);
+        gmarker.addMarker();
+
+        if(!markerGroups[states[spot.status]]) markerGroups[states[spot.status]] = [];
+        markerGroups[states[spot.status]].push(gmarker.marker);
       }
     }
+
+    var legend = document.getElementById('legend');
+    for (var key in gmarker.icons) {
+      var type = gmarker.icons[key];
+      var name = type.name;
+      var icon = type.icon;
+      var div = document.createElement('div');
+      div.innerHTML = '<img src="' + icon + '"> ' + name;
+      legend.appendChild(div);
+    }
+
   });
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
 }
+
+function toggleGroup(type){
+  for (var i = 0; i < markerGroups[type].length; i++) {
+    var marker = markerGroups[type][i];
+    if (!marker.getVisible()) {
+      marker.setVisible(true);
+    } else {
+      marker.setVisible(false);
+    }
+  }
+}
+
 
 function configureMapSize() {
   var viewportWidth = $(window).width();
@@ -32,6 +68,8 @@ function configureMapSize() {
     $("#map").css({ height: (viewportHeight - 64) + 'px' });
   }
 }
+
+
 
 $(window).load(function() {
   configureMapSize();
--- app/assets/javascripts/marker.js
+++ app/assets/javascripts/marker.js
@@ -1,10 +1,35 @@
 function GmapMarker (gmap, pos) {
   this.map = gmap;
-  this.position = new google.maps.LatLng(pos.lat, pos.lng);
-  this.iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-  this.marker = new google.maps.Marker({
-     map: this.map,
-     position: this.position,
-     icon: this.iconBase + 'parking_lot_maps.png'
-  });
+  this.iconBase = 'spot_icons/';
+  this.icons = {
+    defected: {
+      name: 'Defected',
+      icon: this.iconBase + 'spot-gray.png'
+    },
+    available: {
+      name: 'Available',
+      icon: this.iconBase + 'spot-green.png'
+    },
+    occupied: {
+      name: 'Occupied',
+      icon: this.iconBase + 'spot-red.png'
+    }
+  };
+
+  var spot_states = { '-1': 'defected', '0': 'available', '1': 'occupied'};
+
+  this.feature = {
+      position: new google.maps.LatLng(pos.lat, pos.lng),
+      state: spot_states[pos.status]
+  };
+
+  this.marker = null;
+  this.addMarker = function () {
+    this.marker = new google.maps.Marker({
+      map: this.map,
+      position: this.feature.position,
+      icon: this.icons[this.feature.state].icon
+    });
+  };
+
 }
