4a5c9d322a2d14a65895cc319045630de9040119
Thiago Petrone <thiago.petrone@gmail.com>
2016-06-07 14:29:15 -0300

    Fix JsHint warnings

--- app/assets/javascripts/auto_refresh.js
+++ app/assets/javascripts/auto_refresh.js
@@ -1,13 +1,12 @@
 $(function () {
   var refresh;
   $("#switch-1, #time-slider").on('click', function(){
-    if($("#switch-1").prop('checked') == true){
+    if($("#switch-1").prop('checked') === true){
       time = $("#time-slider").prop("value")* 1000;
       clearInterval(refresh);
       refresh = setInterval(function(){
         get_spots(map);
-        }
-      , time);
+        }, time);
     }else{
       clearInterval(refresh);
     }
