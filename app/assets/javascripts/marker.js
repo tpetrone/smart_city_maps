6314befa699d20d34ae97546641de6ca4615ebe7
Debora Setton <setton.debora@gmail.com>
2016-06-08 20:46:15 -0300

    Do not trace route automatically (wait for button click)

--- app/assets/javascripts/marker.js
+++ app/assets/javascripts/marker.js
@@ -4,42 +4,21 @@
 function GmapMarker (gmap, spotData) {
   var self = this;
   this.map = gmap;
-  this.iconBase = 'spot_icons/';
-  this.icons = {
-    defected: {
-      name: 'Defected',
-      icon: this.iconBase + 'spot-gray.png'
-    },
-    available: {
-      name: 'Available',
-      icon: this.iconBase + 'spot-green.png'
-    },
-    occupied: {
-      name: 'Occupied',
-      icon: this.iconBase + 'spot-red.png'
-    }
-  };
 
   this.addMarker = function (spotData) {
     var position = new google.maps.LatLng(spotData.latitude, spotData.longitude);
     var state = Spot.STATUSES[spotData.status.toString()];
     var _data = spotData;
 
-  this.addMarker = function () {
     self.marker = new google.maps.Marker({
       map: this.map,
       position: position,
       icon: GmapMarker.ICONS[state].icon
     });
 
-    self.marker.addListener('click', function() {
-      traceroute(self.map, self.marker.position);
-    });
-
     var detail = new Detail();
     detail.showInfo(gmap, this.marker, _data);
   };
-
 }
 
 GmapMarker.ICONS = {
