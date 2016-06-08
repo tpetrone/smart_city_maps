dd16cb6b322dd441e8462bfe9302b650868cefa8
Thiago Petrone <thiago.petrone@gmail.com>
2016-06-08 02:07:11 -0300

    Refactor auto-refresh, fix getSpots method, add comments

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,3 +1,11 @@
+$(function () {
+  $("#switch-1, #time-slider").on('click', function(){
+    var switch_state = $("#switch-1").prop('checked');
+    var slider_value = $("#time-slider").prop("value");
+    map.autoRefresh(switch_state,slider_value);
+  });
+});
+
 // Initial coordinates on which to center the map when the page is loaded.
 INIT_LAT = -23.545241799982602;
 INIT_LNG = -46.63861848413944;
@@ -15,6 +23,7 @@ function setupGmapClass() {
       },
       zoom: INIT_ZOOM
     });
+    this.refresh = null;
   };
 
   // Make Gmap "inherit" from google.maps.Map.
@@ -31,18 +40,46 @@ function setupGmapClass() {
     });
   };
 
-  Gmap.prototype.getSpots = function(filterManager){
+  /**
+    * Method to request the spots
+    * create a marker and
+    * assign it to AvailabilityFilter
+    */
+  Gmap.prototype.getSpots = function(){
     $.getJSON("/json/spots.json").done(function(data) {
+      gmarker = new GmapMarker(map);
+      filterManager.resetAll();
       if (data.spots.length > 0) {
         for (i = 0; i < data.spots.length; i++) {
           spot = data.spots[i];
-          gmarker = new GmapMarker(map);
           gmarker.addMarker(spot);
-
-          filterManager.assignSpot(spot);
+          filterManager.assignSpot(spot,gmarker.marker);
         }
       }
     });
+  };
+
+  /**
+    * Method to toggle auto refresh
+    * feature and set the refresh interval
+    * according to the slider in the UI
+    */
+  Gmap.prototype.autoRefresh = function(switch_state, slider_value){
+    var map = this;
+    if(switch_state === true){
+      time = slider_value * 1000;
+      clearInterval(this.refresh);
+      this.refresh = setInterval(function(){
+        map.getSpots();
+        /**
+         * It is not clear if the location should be refreshed here.
+         * UX tests must be made
+         */
+        getUserLocation(map);
+        }, time);
+    }else{
+      clearInterval(this.refresh);
+    }
   };
 
 }
