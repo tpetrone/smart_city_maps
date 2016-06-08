7b13a49e7202546db0ccd301e0e5d51fef2c964c
Debora Setton <setton.debora@gmail.com>
2016-06-08 18:34:10 -0300

    Refactor #15

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,53 +1,119 @@
 $(function () {
   // Prevent the page from reloading.
-  $("#header-search").submit(function(event) {
-     event.preventDefault();
+  $('#header-search').submit(function (event) {
+    event.preventDefault();
   });
 });
 
 /**
  * Repositions the map when the user selects a location.
  */
-function placeChangedHandler(autocomplete, map) {
+function placeChangedHandler(place, map) {
+  if (!hasGeometry(place)) return;
 
-  var infowindow = new google.maps.InfoWindow();
-  var place = autocomplete.getPlace();
-  if (!place.geometry) {
-    return;
+  var isCenteredByViewport = centerMapByViewport(place.geometry.viewport, map);
+  if (!isCenteredByViewport) {
+    centerMapByLocation(place.geometry.location, map);
   }
-  // If the place has a geometry, then present it on a map.
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(15);
+  var marker = createMarker(0, -29, map);
+  setupMarkerIcon(place, marker);
+  showInfoWindow(map, marker, place);
+  showEstabSpotsOnly(place);
+}
+
+function hasGeometry(place) {
+  if (!place || !place.geometry) {
+    return false;
   }
+  return true;
+}
 
-  marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
+function centerMapByViewport(viewport, map) {
+  result = false;
+  try {
+    map.fitBounds(viewport);
+    result = true;
+  } catch(err) { }
+  return result;
+}
 
-  marker.setIcon(/** @type {google.maps.Icon} */({
-    url: place.icon,
-    size: new google.maps.Size(71, 71),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(17, 34),
-    scaledSize: new google.maps.Size(35, 35)
-  }));
+function centerMapByLocation(location, map) {
+  map.setCenter(location);
+  map.setZoom(15);
+}
 
+function setupMarkerIcon(place, marker) {
+  if (place.icon) {
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+  }
   marker.setPosition(place.geometry.location);
   marker.setVisible(true);
+}
 
+function showInfoWindow(map, marker, place) {
+  var address = getAddress(place.address_components);
+  var infowindow = new google.maps.InfoWindow();
+  infowindow.setContent('<div><strong>' + getPlaceName(place) + '</strong><br>' + address);
+  infowindow.open(map, marker);
+}
+
+function getPlaceName(place) {
+  return (place.name != undefined) ? place.name : "Unnamed";
+}
+
+function getAddress(address_components) {
   var address = '';
-  if (place.address_components) {
+  if (address_components) {
     address = [
-      (place.address_components[0] && place.address_components[0].short_name || ''),
-      (place.address_components[1] && place.address_components[1].short_name || ''),
-      (place.address_components[2] && place.address_components[2].short_name || '')
+      (address_components[0] && address_components[0].short_name || ''),
+      (address_components[1] && address_components[1].short_name || ''),
+      (address_components[2] && address_components[2].short_name || '')
     ].join(' ');
   }
+  return address;
+}
 
-  infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-  infowindow.open(map, marker);
+function createMarker(x, y, map) {
+  return new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(x, y)
+  });
+}
+
+function showEstabSpotsOnly(place) {
+  $('#establishment-controls').hide();
+
+  if (place.types) {
+    for (i = 0; i < place.types.length; i++) {
+      if (place.types[i] == "establishment") {
+
+        $('#establishment-controls').show();
+        $('#establishment-controls input').change(function () {
+          filterManager.allMarkers;
+
+          var showOther = !this.checked;
+
+          for (var i = 0; i < filterManager.allMarkers.length; i++) {
+            var obj = filterManager.allMarkers[i];
+            if (showOther) {
+              obj.marker.marker.setVisible(true);
+            }
+            else {
+              console.log("Comparing " + obj.spot.google_establishment_id + " with " + place.place_id);
+              if (obj.spot.google_establishment_id !== place.place_id) {
+                obj.marker.marker.setVisible(false);
+              }
+            }
+          }
+        });
+
+      }
+    }
+  }
 }
--- spec/casperjs/features/_autocomplete_spec.js
+++ spec/casperjs/features/_autocomplete_spec.js
@@ -11,7 +11,94 @@ exports.spec = function(casper, test, other) {
     test.assert(typeof(map) === "object", "Map is loaded");
   });
 
+  // Tests the creation of the autocomplete object
   casper.then(function() {
-    // TODO!
+    var autocomplete = casper.evaluate(function() {
+      input = $("#search-field")[0];
+      return setupAutocomplete(input, window.map);
+    });
+    test.assert(typeof(autocomplete) === "object", "Autocomplete is created");
+  });
+
+  casper.then(function() {
+    var noGeometry = casper.evaluate(function() {
+      input = $("#search-field")[0];
+      autocomplete = setupAutocomplete(input, window.map);
+      place = autocomplete.getPlace();
+      return hasGeometry(place);
+    });
+    test.assert(noGeometry === false, "Place without geometry");
+  });
+
+  casper.then(function() {
+    casper.evaluate(function() {
+      url_street = 'https://maps.googleapis.com/maps/api/geocode/json?address=abaibas&key=AIzaSyAPm6DbvAvOWQe4FUMDrPfglaCSN_Wzqt4';
+      $.get(url_street).done(function(data) {
+        window.my_street = data;
+      });
+
+      var establishment = {
+         "geometry" : {
+            "location" : {
+               "lat" : -23.5568137,
+               "lng" : -46.73467780000001
+            }
+         },
+         "name" : "Monumento a Ramos de Azevedo",
+         "place_id" : "ChIJr4NMIxFWzpQRT-ZbW7SXmP0",
+         "types" : [ "point_of_interest", "establishment" ]
+      };
+
+      window.my_estab = establishment;
+
+    });
+  });
+
+  casper.wait(1000, function() {
+    casper.evaluate(function() {
+      return window.my_street;
+    });
+  });
+
+  casper.wait(1000, function() {
+    casper.evaluate(function() {
+      return window.my_estab;
+    });
+  });
+
+  casper.then(function() {
+    casper.evaluate(function() {
+      window.place = window.my_street.results[0];
+      return window.place;
+    });
+  });
+
+  casper.then(function() {
+    casper.evaluate(function() {
+      window.estab = window.my_estab;
+      return window.estab;
+    });
+  });
+
+  casper.then(function() {
+    casper.evaluate(function() {
+      window.estab_str = JSON.stringify(window.estab);
+      return window.estab_str;
+    });
+  });
+
+  casper.then(function() {
+    casper.evaluate(function() {
+      window.hasGeometry(window.place);
+      window.showEstabSpotsOnly(window.estab);
+    });
+  });
+
+  casper.then(function() {
+    var res = casper.evaluate(function() {
+      window.placeChangedHandler(window.place, window.map);
+      return true;
+    });
+    test.assert(res == true, window.estab_str);
   });
 };
