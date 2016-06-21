99401fc8c70af924b08902a289914f9919f7226f
Jefferson O. Silva <silva.o.jefferson@gmail.com>
2016-06-21 17:11:01 -0300

    Handle spots pricing restrictions

---
+++ app/assets/javascripts/parking_restrictions.js
@@ -0,0 +1,81 @@
+$(function () {
+  /*
+   * Listens for changes in min-price element
+   */
+  $("#min-price").change(function() {
+    var newMin = parseFloat($("#min-price").val());
+    if (newMin != NaN) {
+      showSpotsAboveThisPrice(newMin);
+    }
+  });
+
+  /*
+   * Listens for changes in max-price element
+   */
+   $("#max-price").change(function() {
+     var newMax = parseFloat($("#max-price").val());
+     if (newMax != NaN) {
+       showSpotsBelowThisPrice(newMax);
+     }
+   });
+});
+
+/*
+ * Show only spots with prices lower than newMax
+ */
+function showSpotsBelowThisPrice(newMax) {
+  var allMarkersLength = filterManager.allMarkers.length;
+  for (var i = 0; i < allMarkersLength; i++) {
+    var obj = filterManager.allMarkers[i];
+    var pricingRestrictions = obj.spot.formatted_details.pricing_restrictions[0];
+
+    // If the spot has any pricing restrictions
+    if (pricingRestrictions != undefined) {
+      spotPrice = getSpotPrice(pricingRestrictions);
+      if (spotPrice <= newMax) {
+        obj.marker.marker.setVisible(true);
+      } else {
+        obj.marker.marker.setVisible(false);
+      }
+    }
+  }
+}
+
+/*
+ * Extract spot price from pricing restrictions string
+ */
+function getSpotPrice(pricingRestrictions) {
+  var PRICE_REGEX = /[0-9]+\.[0-9][0-9]/;
+  var spotPrice = null;
+  try {
+    var i = pricingRestrictions.search(PRICE_REGEX);
+    var j = pricingRestrictions.length;
+    var priceStr = pricingRestrictions.substring(i,j);
+    spotPrice = parseFloat(priceStr);
+  } catch(e) {
+    console.log("Error parsing spot price to float: " + pricingRestrictions);
+    throw e;
+  }
+  return spotPrice;
+}
+
+/*
+ * Shows only spots with prices higher than newMin
+ */
+function showSpotsAboveThisPrice(newMin) {
+  var allMarkersLength = filterManager.allMarkers.length;
+  for (var i = 0; i < allMarkersLength; i++) {
+    var obj = filterManager.allMarkers[i];
+    var pricingRestrictions = obj.spot.formatted_details.pricing_restrictions[0];
+
+    // If the spot has any pricing restrictions
+    if (pricingRestrictions != undefined) {
+      spotPrice = getSpotPrice(pricingRestrictions);
+      if (spotPrice >= newMin) {
+        obj.marker.marker.setVisible(true);
+      } else {
+        obj.marker.marker.setVisible(false);
+      }
+    }
+  }
+}
